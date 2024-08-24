0e6e147935f7706ec462ad972aaf209f9d8ab2ff
Akshat Saini <axshat.ind@gmail.com>
2024-08-24 23:21:20 +0530

    import it to dashboard in page.jsx of dashboard

--- app/(routes)/dashboard/page.js
+++ app/(routes)/dashboard/page.js
@@ -1,11 +1,14 @@
 import { UserButton } from "@clerk/nextjs";
 import React from "react";
 import Header from "./_components/Header";
+import WorkspaceList from "./_components/WorkspaceList";
 
 function Dashboard() {
   return (
     <div>
       <Header />
+
+      <WorkspaceList />
     </div>
   );
 }
