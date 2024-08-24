93c4e48a2b161f1242187267b84e5960f48bfd8c
Akshat Saini <axshat.ind@gmail.com>
2024-08-25 03:59:57 +0530

    gave hover effect on cover

--- app/(routes)/createworkspace/page.jsx
+++ app/(routes)/createworkspace/page.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
 
 function CreateWorkspace() {
-  return <div>CreateWorkspace</div>;
+  const [coverImage, setCoverImage] = useState("/cover.png");
+
+  return (
+    <div className="p-10 md:px-36 lg:px-52 xl:px-80 py-20">
+      <div>
+        {/* Cover Image */}
+        <div className="relative group">
+          <h2 className="hidden absolute p-4 w-full h-full items-center group-hover:flex justify-center text-white">
+            Change Cover
+          </h2>
+          <div>
+            <Image
+              src={coverImage}
+              width={400}
+              height={440}
+              className="w-full h-[150px] object-cover rounded-t-xl"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CreateWorkspace;
