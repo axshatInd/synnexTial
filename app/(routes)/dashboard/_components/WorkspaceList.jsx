6906a90e0c365d8f3efae4c9895990a2bcb2b42d
Akshat Saini <axshat.ind@gmail.com>
2024-08-25 02:43:07 +0530

    added Image and button to add a new workspace (in middle of screen)

--- app/(routes)/dashboard/_components/WorkspaceList.jsx
+++ app/(routes)/dashboard/_components/WorkspaceList.jsx
@@ -2,10 +2,13 @@
 import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/nextjs";
 import { AlignLeft, LayoutGrid } from "lucide-react";
-import React from "react";
+import Image from "next/image";
+import React, { useState } from "react";
 
 function WorkspaceList() {
   const { user } = useUser();
+
+  const [WorkspaceList, setWorkspaceList] = useState([]);
   return (
     <div className="my-10 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-10 py-4 border border-gray-300 shadow-lg rounded-lg max-w-6xl mx-auto">
       <div className="flex justify-between items-center mb-8">
@@ -23,6 +26,22 @@ function WorkspaceList() {
           <AlignLeft />
         </div>
       </div>
+      {WorkspaceList?.length == 0 ? (
+        <div className="flex flex-col justify-center items-center">
+          <Image
+            src={"/workspace.png"}
+            width={200}
+            height={200}
+            alt="Workspace"
+          />
+          <h2>Create a new Workspace</h2>
+          <Button variant="outline" className="my-3">
+            + New Workspace
+          </Button>
+        </div>
+      ) : (
+        <div>Workspace List</div>
+      )}
     </div>
   );
 }
