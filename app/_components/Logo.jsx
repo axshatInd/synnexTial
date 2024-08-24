ec8ba9e86ce5b984567a92d2b1084da435332d06
Akshat Saini <axshat.ind@gmail.com>
2024-08-24 23:53:38 +0530

    implemented colour to logo with this new method

--- app/_components/Logo.jsx
+++ app/_components/Logo.jsx
@@ -5,9 +5,7 @@ function Logo() {
   return (
     <div className="flex items-center gap-1">
       <Image src={"/logo.svg"} alt="Logo" width={30} height={30} />
-      <h2 className="font-semibold text-xl bg-gradient-to-r from-yellow-500 via-orange-500 to-yellow-700 bg-clip-text text-transparent">
-        synnex
-      </h2>
+      <h2 className="font-semibold text-xl gradient-text">synnex</h2>
     </div>
   );
 }
