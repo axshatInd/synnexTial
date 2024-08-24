520eda4c2ceb06a2b05e1e4a39ae3e2184b77e4a
Akshat Saini <axshat.ind@gmail.com>
2024-08-25 00:27:09 +0530

    changed size of header box

--- app/(routes)/dashboard/_components/WorkspaceList.jsx
+++ app/(routes)/dashboard/_components/WorkspaceList.jsx
@@ -6,8 +6,8 @@ import React from "react";
 function WorkspaceList() {
   const { user } = useUser();
   return (
-    <div className="my-10 p-10 md:px-24 lg:px-36 xl:px-52">
-      <div className="flex justify-between">
+    <div className="my-10 px-4 sm:px-6 md:px-8 lg:px-10 xl:px-10 py-4 border border-gray-300 shadow-lg rounded-lg max-w-6xl mx-auto">
+      <div className="flex justify-between items-center">
         <h2 className="font-semibold text-xl gradient-text3">
           Hello, {user?.username}!
         </h2>
