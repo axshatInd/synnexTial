8c28e85cf492cb2185f2db005fd9855f2e0fd66e
Akshat Saini <axshat.ind@gmail.com>
2024-08-24 23:34:17 +0530

    Hello user

--- app/(routes)/dashboard/_components/WorkspaceList.jsx
+++ app/(routes)/dashboard/_components/WorkspaceList.jsx
@@ -1,7 +1,14 @@
+"use client";
+import { useUser } from "@clerk/nextjs";
 import React from "react";
 
 function WorkspaceList() {
-  return <div>WorkspaceList</div>;
+  const { user } = useUser();
+  return (
+    <div className="my-10 p-10 md:px-24 lg:px-36 xl:px-52">
+      <h2>Hello, {user?.fullName}</h2>
+    </div>
+  );
 }
 
 export default WorkspaceList;
