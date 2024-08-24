ca751e03f6e792e5d00887bdf3d93e85fa57d118
Akshat Saini <axshat.ind@gmail.com>
2024-08-25 04:54:24 +0530

    removed h2 tag of renaming mention

--- app/(routes)/createworkspace/page.jsx
+++ app/(routes)/createworkspace/page.jsx
@@ -30,10 +30,11 @@ function CreateWorkspace() {
         {/*Input Section */}
         <div className="p-12">
           <h2 className="font-medium text-xl">Create a new Workspace </h2>
-          <h2 className="text-sm mt-2">
-            Synnex lets you create a shared space to collaborate with your team.
-            <h2>You can always rename it later!</h2>
+          <h2 className="text-lg mt-2 text-gray-700">
+            <span className="font-medium">Synnex</span> lets you create a shared
+            space to collaborate with your team.
           </h2>
+
           <div className="mt-8 flex gap-2 items-center">
             <Button variant="outline">
               <SmilePlus />
