52443eb8440a8d30e68fef8e6734f2f642449c44
Akshat Saini <axshat.ind@gmail.com>
2024-08-24 22:39:28 +0530

    sorted a mistake of creating logo component in Header.jsx instead of Logo.jsx, imported <Logo/> in Header.jsx

--- app/(routes)/dashboard/_components/Header.jsx
+++ app/(routes)/dashboard/_components/Header.jsx
@@ -1,11 +1,10 @@
 import Logo from "@/app/_components/Logo";
-import Image from "next/image";
 import React from "react";
 
 function Header() {
   return (
     <div>
-      <Image src={"/logo.svg"} alt="Logo" width={30} height={30} />
+      <Logo />
     </div>
   );
 }
--- app/_components/Logo.jsx
+++ app/_components/Logo.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Image from "next/image";
 
 function Logo() {
-  return <div>Logo</div>;
+  return (
+    <div className="flex items-center gap-1">
+      <Image src={"/logo.svg"} alt="Logo" width={30} height={30} />
+      <h2 className="font-semibold text-xl">synnex</h2>
+    </div>
+  );
 }
 
 export default Logo;
