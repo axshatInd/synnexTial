db9a3fb80367c178face3a1d66bef16386b6960f
Akshat Saini <axshat.ind@gmail.com>
2024-08-24 23:17:18 +0530

    Organization setup done

--- app/(routes)/dashboard/_components/Header.jsx
+++ app/(routes)/dashboard/_components/Header.jsx
@@ -1,12 +1,18 @@
+"use client";
 import Logo from "@/app/_components/Logo";
-import { UserButton } from "@clerk/nextjs";
+import { OrganizationSwitcher, useAuth, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 function Header() {
+  const { orgId } = useAuth();
+  console.log(orgId);
   return (
     <div className="flex justify-between items-center p-3 shadow-xl">
       <Logo />
-
+      <OrganizationSwitcher
+        afterCreateOrganizationUrl={"/dashboard"}
+        afterLeaveOrganizationUrl={"/dashboard"}
+      />
       <UserButton />
     </div>
   );
