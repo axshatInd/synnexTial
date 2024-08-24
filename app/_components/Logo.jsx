da2efddbb02a910a50b0e764fa2dd7cb9356d1ba
Akshat Saini <axshat.ind@gmail.com>
2024-08-24 23:50:03 +0530

    logo in orange gradient

--- app/_components/Logo.jsx
+++ app/_components/Logo.jsx
@@ -5,7 +5,9 @@ function Logo() {
   return (
     <div className="flex items-center gap-1">
       <Image src={"/logo.svg"} alt="Logo" width={30} height={30} />
-      <h2 className="font-semibold text-xl">synnex</h2>
+      <h2 className="font-semibold text-xl bg-gradient-to-r from-yellow-500 via-orange-500 to-yellow-700 bg-clip-text text-transparent">
+        synnex
+      </h2>
     </div>
   );
 }
