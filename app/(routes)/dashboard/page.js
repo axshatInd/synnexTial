09d976594372f914812abc3c30842646b6fd3ebd
Akshat Saini <axshat.ind@gmail.com>
2024-08-24 22:09:54 +0530

    created Header.jsx in Dashboard

--- app/(routes)/dashboard/page.js
+++ app/(routes)/dashboard/page.js
@@ -2,11 +2,7 @@ import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 function Dashboard() {
-  return (
-    <div>
-      <UserButton />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default Dashboard;
