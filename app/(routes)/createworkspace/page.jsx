0cc55b7d364ae209285ab4b27be0a3a8783a0960
Akshat Saini <axshat.ind@gmail.com>
2024-08-25 04:13:22 +0530

    Added emoji button

--- app/(routes)/createworkspace/page.jsx
+++ app/(routes)/createworkspace/page.jsx
@@ -1,4 +1,6 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import { SmilePlus } from "lucide-react";
 import Image from "next/image";
 import React, { useState } from "react";
 
@@ -9,7 +11,7 @@ function CreateWorkspace() {
     <div className="p-10 md:px-36 lg:px-52 xl:px-80 py-20">
       <div>
         {/* Cover Image */}
-        <div className="relative group">
+        <div className="relative group cursor-pointer">
           <h2 className="hidden absolute p-4 w-full h-full items-center group-hover:flex justify-center text-black">
             Change Cover
           </h2>
@@ -20,6 +22,20 @@ function CreateWorkspace() {
               height={440}
               className="w-full h-[150px] object-cover rounded-t-xl"
             />
+          </div>
+        </div>
+
+        {/*Input Section */}
+        <div className="p-12">
+          <h2 className="font-medium text-xl">Create a new Workspace </h2>
+          <h2 className="text-sm mt-2">
+            Synnex lets you create a shared space to collaborate with your team.
+            <h2>You can always rename it later!</h2>
+          </h2>
+          <div>
+            <Button variant="outline">
+              <SmilePlus />
+            </Button>
           </div>
         </div>
       </div>
