fcef5485911c9ea0a2232569d7d3bbe0e1169bd9
Akshat Saini <axshat.ind@gmail.com>
2024-08-24 23:41:19 +0530

    added button component, + button

--- app/(routes)/dashboard/_components/WorkspaceList.jsx
+++ app/(routes)/dashboard/_components/WorkspaceList.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/nextjs";
 import React from "react";
 
@@ -6,8 +7,9 @@ function WorkspaceList() {
   const { user } = useUser();
   return (
     <div className="my-10 p-10 md:px-24 lg:px-36 xl:px-52">
-      <div>
-        <h2>Hello, {user?.username} </h2>
+      <div className="flex justify-between">
+        <h2 className="font-semibold text-xl">Hello, {user?.username}</h2>
+        <Button>+</Button>
       </div>
     </div>
   );
