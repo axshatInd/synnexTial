7423d26b2ed8f10a4ecff6556ae96e629f256805
Akshat Saini <axshat.ind@gmail.com>
2024-08-24 23:37:28 +0530

    Hello, username

--- app/(routes)/dashboard/_components/WorkspaceList.jsx
+++ app/(routes)/dashboard/_components/WorkspaceList.jsx
@@ -6,7 +6,9 @@ function WorkspaceList() {
   const { user } = useUser();
   return (
     <div className="my-10 p-10 md:px-24 lg:px-36 xl:px-52">
-      <h2>Hello, {user?.fullName}</h2>
+      <div>
+        <h2>Hello, {user?.username} </h2>
+      </div>
     </div>
   );
 }
