1762a968ec4b5ade35cbb9a555f5fefb14432b13
Akshat Saini <axshat.ind@gmail.com>
2024-08-25 17:34:26 +0530

    imported component (fogot to do)

--- app/(routes)/createworkspace/page.jsx
+++ app/(routes)/createworkspace/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import CoverPicker from "@/app/_components/CoverPicker";
+import EmojiPickerComponent from "@/app/_components/EmojiPickerComponent";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { SmilePlus } from "lucide-react";
