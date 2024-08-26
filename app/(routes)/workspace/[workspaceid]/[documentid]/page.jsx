32c04d9f3b62321eb81638bbd066484826feaa29
Akshat Saini <axshat.ind@gmail.com>
2024-08-27 04:04:07 +0530

    workspace document created (page.jsx) in [documentid]

---
+++ app/(routes)/workspace/[workspaceid]/[documentid]/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function WorkspaceDocument() {
+  return <div>WorkspaceDocument</div>;
+}
+
+export default WorkspaceDocument;
--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h2>Subscribe</h2>
-      <Button> Subscribe </Button>
+      <Link href="/dashboard" passHref>
+        <Button> Subscribe </Button>
+      </Link>
     </div>
   );
 }
