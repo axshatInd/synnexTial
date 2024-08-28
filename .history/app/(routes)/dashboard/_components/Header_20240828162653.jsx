37461da903cd45fd33719b22108b0ab8054fd9c7
Akshat Saini <axshat.ind@gmail.com>
2024-08-28 16:31:37 +0530

    Header is better now just need to sort the svg logo

--- .history/app/(routes)/dashboard/_components/Header_20240828162653.jsx
+++ .history/app/(routes)/dashboard/_components/Header_20240828162653.jsx
@@ -3,6 +3,7 @@ import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import Logo from "@/app/_components/Logo";
 import { OrganizationSwitcher, useAuth, UserButton } from "@clerk/nextjs";
+// import ThemeSwitcher from "@/app/_components/ThemeSwitcher"; // Removed
 import useTheme from "@/app/_components/useTheme"; // Import useTheme here
 
 function Header() {
@@ -19,11 +20,9 @@ function Header() {
   return (
     <header
       ref={headerRef} // Reference for animation
-      className={`fixed top-4 left-1/2 transform -translate-x-1/2 z-50 flex justify-between items-center px-4 py-3 md:px-6 md:py-3 shadow-xl rounded-full bg-white w-full max-w-7xl overflow-hidden ${
-        theme === "dark" ? "dark:bg-gray-800" : ""
-      }`}
+      className="fixed top-4 left-1/2 right-1/2 transform -translate-x-1/2 z-50 flex justify-between items-center px-4 py-3 md:px-6 md:py-3 shadow-xl rounded-full bg-white w-full max-w-7xl"
     >
-      <div className="w-full max-w-screen-xl px-4 flex justify-between items-center">
+      <div className="w-full max-w-screen-xl mx-auto px-4 flex justify-between items-center">
         <Logo />
         <OrganizationSwitcher
           afterCreateOrganizationUrl={"/dashboard"}
