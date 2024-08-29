a6bc58ab20bf6deb36564cfe312e65607a286411
Akshat Saini <axshat.ind@gmail.com>
2024-08-30 03:09:57 +0530

    dropdown initialized

--- app/(routes)/workspace/_components/DocumentOptions.jsx
+++ app/(routes)/workspace/_components/DocumentOptions.jsx
@@ -1,10 +1,30 @@
 import { MoreVertical } from "lucide-react";
 import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 function DocumentOptions() {
   return (
     <div>
-      <MoreVertical className="h-3 w-3" />
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <MoreVertical className="h-3 w-3" />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 }
