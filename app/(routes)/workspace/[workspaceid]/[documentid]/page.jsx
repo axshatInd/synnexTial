9109d3291915d196af2f2b77d801e45ae8e662a0
Akshat Saini <axshat.ind@gmail.com>
2024-08-30 21:54:55 +0530

    responsive

--- app/(routes)/workspace/[workspaceid]/[documentid]/page.jsx
+++ app/(routes)/workspace/[workspaceid]/[documentid]/page.jsx
@@ -6,12 +6,10 @@ function WorkspaceDocument({ params }) {
   return (
     <div className="flex h-screen">
       {/* Sidebar */}
-      <div className="fixed">
-        <SideNav params={params} />
-      </div>
+      <SideNav params={params} />
 
       {/* Main Content Area */}
-      <div className="flex-1 ml-72 p-6 mt-16 md:mt-4">
+      <div className="flex-1 md:ml-2 ml-0 p-6 md:mt-4">
         <div className="bg-white dark:bg-black rounded-lg p-4 shadow-md h-full">
           Document
         </div>
