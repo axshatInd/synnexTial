6a6fb099c1daba526f24b0c356074a455fb7416c
Akshat Saini <axshat.ind@gmail.com>
2024-08-30 12:41:09 +0530

    progress bar +100 state

--- app/(routes)/workspace/_components/DocumentList.jsx
+++ app/(routes)/workspace/_components/DocumentList.jsx
@@ -30,7 +30,7 @@ function DocumentList({ documentList, params }) {
     const interval = setInterval(() => {
       setProgress((prev) => {
         if (prev < 100) {
-          return prev + 25; // Increase progress by 25
+          return prev + 100; // Increase progress by 25
         }
         return prev;
       });
