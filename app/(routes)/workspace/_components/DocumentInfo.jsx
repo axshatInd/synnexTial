3db417e4a9aadd43f8531955e2969c4b0a16262d
Akshat Saini <axshat.ind@gmail.com>
2024-08-31 17:48:07 +0530

    Emoji Picker fully set up

--- app/(routes)/workspace/_components/DocumentInfo.jsx
+++ app/(routes)/workspace/_components/DocumentInfo.jsx
@@ -1,14 +1,16 @@
 import CoverPicker from "@/app/_components/CoverPicker";
+import EmojiPickerComponent from "@/app/_components/EmojiPickerComponent";
+import { SmilePlus } from "lucide-react";
 import Image from "next/image";
 import React, { useState } from "react";
 
 function DocumentInfo() {
   const [coverImage, setCoverImage] = useState("/cover.png");
+  const [emoji, setEmoji] = useState(); // State to store selected emoji
 
   return (
     <div>
       {/* Cover Image */}
-
       <CoverPicker setNewCover={(cover) => setCoverImage(cover)}>
         <div className="relative shadow-2xl rounded-xl group">
           {/* Cover Image */}
@@ -28,6 +30,17 @@ function DocumentInfo() {
       </CoverPicker>
 
       {/* Emoji Picker */}
+      <div className="absolute mt-[-40px] cursor-pointer">
+        <EmojiPickerComponent setEmojiIcon={(emoji) => setEmoji(emoji)}>
+          <div className="p-4 rounded-md">
+            {emoji ? (
+              <span className="text-3xl">{emoji}</span>
+            ) : (
+              <SmilePlus className="h-5 w-5 text-gray-500" />
+            )}
+          </div>
+        </EmojiPickerComponent>
+      </div>
 
       {/* File Name */}
     </div>
