fbb5355f83dc9ada2c8f02b86a9a703196721b9d
Akshat Saini <axshat.ind@gmail.com>
2024-09-03 20:01:12 +0530

    rich document

--- app/(routes)/workspace/_components/RichDocumentEditor.jsx
+++ app/(routes)/workspace/_components/RichDocumentEditor.jsx
@@ -6,8 +6,21 @@ import Warning from "@editorjs/warning";
 import Delimiter from "@editorjs/delimiter";
 import Alert from "editorjs-alert";
 import ToggleBlock from "editorjs-toggle-block";
-import List from "@editorjs/list";
+import List from "@editorjs/list"; // import List from "@editorjs/list";
 import NestedList from "@editorjs/nested-list";
+import Checklist from "@editorjs/checklist";
+import editorjsNestedChecklist from "@calumk/editorjs-nested-checklist";
+
+import ImageTool from "@editorjs/image";
+import SimpleImage from "@editorjs/simple-image";
+import LinkTool from "@editorjs/link";
+import AttachesTool from "@editorjs/attaches";
+import Table from "@editorjs/table";
+import CodeTool from "@editorjs/code";
+import RawTool from "@editorjs/raw";
+import CodeBox from "@bomdi/codebox";
+import Marker from "@editorjs/marker";
+import InlineCode from "@editorjs/inline-code";
 
 function RichDocumentEditor() {
   const ref = useRef();
@@ -66,6 +79,74 @@ function RichDocumentEditor() {
             inlineToolbar: true,
             config: {
               defaultStyle: "unordered",
+            },
+          },
+
+          checklist: {
+            class: Checklist,
+            inlineToolbar: true,
+          },
+          nestedchecklist: editorjsNestedChecklist,
+          toggle: {
+            class: ToggleBlock,
+            inlineToolbar: true,
+          },
+
+          inlineCode: {
+            class: InlineCode,
+            shortcut: "CMD+SHIFT+M",
+          },
+          codeBox: {
+            class: CodeBox,
+            config: {
+              themeURL:
+                "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.18.1/build/styles/dracula.min.css", // Optional
+              themeName: "atom-one-dark", // Optional
+              useDefaultTheme: "light", // Optional. This also determines the background color of the language select drop-down
+            },
+          },
+          Marker: {
+            class: Marker,
+            shortcut: "CMD+SHIFT+M",
+          },
+          raw: RawTool,
+          code: CodeTool,
+          attaches: {
+            class: AttachesTool,
+            config: {
+              endpoint: "http://localhost:8008/uploadFile",
+            },
+          },
+          image: SimpleImage,
+          list: {
+            class: List,
+            inlineToolbar: true,
+            config: {
+              defaultStyle: "unordered",
+            },
+          },
+          table: {
+            class: Table,
+            inlineToolbar: true,
+            config: {
+              rows: 2,
+              cols: 3,
+            },
+          },
+          linkTool: {
+            class: LinkTool,
+            config: {
+              endpoint: "http://localhost:8008/fetchUrl", // Your backend endpoint for url data fetching,
+            },
+          },
+
+          image: {
+            class: ImageTool,
+            config: {
+              endpoints: {
+                byFile: "http://localhost:8008/uploadFile", // Your backend file uploader endpoint
+                byUrl: "http://localhost:8008/fetchUrl", // Your endpoint that provides uploading by Url
+              },
             },
           },
         },
