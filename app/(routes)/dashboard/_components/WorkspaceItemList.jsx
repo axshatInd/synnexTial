3fb6d41c6e6db063eed3576970d8db9f17f3e48d
Akshat Saini <axshat.ind@gmail.com>
2024-09-04 00:42:19 +0530

    images of cover inserted in grid

--- app/(routes)/dashboard/_components/WorkspaceItemList.jsx
+++ app/(routes)/dashboard/_components/WorkspaceItemList.jsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
 import React from "react";
 
-function WorkspaceItemList() {
-  return <div>WorkspaceItemList</div>;
+function WorkspaceItemList({ workspaceList }) {
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      {workspaceList &&
+        workspaceList.map((workspace, index) => (
+          <div key={index}>
+            <Image
+              src={workspace?.coverImage}
+              width={400}
+              height={200}
+              alt="cover"
+              className="h-[150px] object-cover rounded-t-xl"
+            />
+            <h2>{workspace.workspaceName}</h2>
+          </div>
+        ))}
+    </div>
+  );
 }
 
 export default WorkspaceItemList;
