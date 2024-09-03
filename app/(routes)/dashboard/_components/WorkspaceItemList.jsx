8bd7df0b8885384fc82c6211af9067f471f445ed
Akshat Saini <axshat.ind@gmail.com>
2024-09-04 01:22:27 +0530

    clicking on workspace card will take to workspace/workspaceid page (routing)

--- app/(routes)/dashboard/_components/WorkspaceItemList.jsx
+++ app/(routes)/dashboard/_components/WorkspaceItemList.jsx
@@ -1,7 +1,13 @@
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+
 import React from "react";
 
 function WorkspaceItemList({ workspaceList }) {
+  const router = useRouter();
+  const OnClickWorkspaceItem = (workspaceId) => {
+    router.push("/workspace/" + workspaceId);
+  };
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6">
       {workspaceList &&
@@ -9,6 +15,7 @@ function WorkspaceItemList({ workspaceList }) {
           <div
             key={index}
             className="border shadow-xl rounded-xl hover:scale-105 transition-all cursor-pointer"
+            onClick={() => OnClickWorkspaceItem(workspace.id)}
           >
             <Image
               src={workspace?.coverImage}
