7b0853a43f2b7ec4fb16d5d8ab19f8c2acfaf112
Akshat Saini <axshat.ind@gmail.com>
2024-09-04 01:27:30 +0530

    ....fine

--- app/(routes)/dashboard/_components/WorkspaceItemList.jsx
+++ app/(routes)/dashboard/_components/WorkspaceItemList.jsx
@@ -9,7 +9,7 @@ function WorkspaceItemList({ workspaceList }) {
     router.push("/workspace/" + workspaceId);
   };
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6">
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-4">
       {workspaceList &&
         workspaceList.map((workspace, index) => (
           <div
