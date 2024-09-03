25295cc660127ab2a89a298f28d53579895db1fd
Akshat Saini <axshat.ind@gmail.com>
2024-09-03 19:30:28 +0530

    till list pretty good

--- app/(routes)/workspace/_components/RichDocumentEditor.jsx
+++ app/(routes)/workspace/_components/RichDocumentEditor.jsx
@@ -1,6 +1,13 @@
 import React, { useEffect, useRef } from "react";
 import EditorJS from "@editorjs/editorjs";
 
+import Header from "@editorjs/header";
+import Warning from "@editorjs/warning";
+import Delimiter from "@editorjs/delimiter";
+import Alert from "editorjs-alert";
+import ToggleBlock from "editorjs-toggle-block";
+import List from "@editorjs/list";
+
 function RichDocumentEditor() {
   const ref = useRef();
   let editor;
@@ -14,7 +21,53 @@ function RichDocumentEditor() {
          * Id of Element that should contain Editor instance
          */
         holder: "editorjs",
-        tools: {},
+        tools: {
+          header: Header,
+          warning: {
+            class: Warning,
+            inlineToolbar: true,
+            shortcut: "CMD+SHIFT+W",
+            config: {
+              titlePlaceholder: "Title",
+              messagePlaceholder: "Message",
+            },
+          },
+
+          delimiter: Delimiter,
+
+          alert: {
+            class: Alert,
+            inlineToolbar: true,
+            shortcut: "CMD+SHIFT+A",
+            config: {
+              alertTypes: [
+                "primary",
+                "secondary",
+                "info",
+                "success",
+                "warning",
+                "danger",
+                "light",
+                "dark",
+              ],
+              defaultType: "primary",
+              messagePlaceholder: "Enter something",
+            },
+          },
+
+          toggle: {
+            class: ToggleBlock,
+            inlineToolbar: true,
+          },
+
+          list: {
+            class: List,
+            inlineToolbar: true,
+            config: {
+              defaultStyle: "unordered",
+            },
+          },
+        },
       });
       ref.current = editor;
     }
