937e25881cce3441bf12276bed9cabb1e5bb9130
Akshat Saini <axshat.ind@gmail.com>
2024-09-05 05:35:09 +0530

    !sharebutton

--- app/(routes)/workspace/_components/DocumentHeader.jsx
+++ app/(routes)/workspace/_components/DocumentHeader.jsx
@@ -5,7 +5,7 @@ function DocumentHeader() {
   return (
     <div>
       <div className="z-10 flex gap-2 justify-end pr-3 rounded-xl">
-        <Button className="py-1 px-3 text-sm">Share</Button>
+        {/* <Button className="py-1 px-3 text-sm">Share</Button> */}
       </div>
     </div>
   );
